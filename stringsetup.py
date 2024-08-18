8c8139519763ce1908827a6b5e415598fc9c8775
RepthonUserbot <124282236+xxe9@users.noreply.github.com>
2024-08-18 18:01:49 +0300

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -16,6 +16,6 @@ Create a new application, by entering the required details"""
 API_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
-with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     print(client.session.save())
     client.send_message("me", client.session.save())
