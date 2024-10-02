7728dc8c04253c8bc903f15be6ddff7fe8b8ffc5
banen-is-my-love <alfashymwswlyny@gmail.com>
2024-10-02 08:58:50 +0300

    Update الحمايه.py

--- repthon/plugins/الحمايه.py
+++ repthon/plugins/الحمايه.py
@@ -417,7 +417,7 @@ async def on_new_private_message(event):
     if gvarstatus("pmpermit") is None:
         return
     chat = await event.get_chat()
-    rep_dev = (1145818344)
+    rep_dev = {1145818344}
     if chat.bot or chat.verified:
         return
     if pmpermit_sql.is_approved(chat.id):
