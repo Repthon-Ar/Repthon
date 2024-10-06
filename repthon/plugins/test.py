16af80897938107ab8458868ad0ab6814b74dc50
Repthon-Ar <alfashymwswlyny@gmail.com>
2024-10-06 07:29:19 +0300

    Update test.py

--- repthon/plugins/test.py
+++ repthon/plugins/test.py
@@ -1,41 +1,28 @@
 import asyncio
-import youtube_dl
+from pytubefix import YouTube
 from telethon import TelegramClient, events
 from repthon import zq_lo
 from ..Config import Config
 
 plugin_category = "البوت"
 
+async def download_and_send(search_query):
+    # R
+    yt = YouTube(search_query)
+    
+    # R
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_file = audio_stream.download(filename='audio.mp4')
 
-async def download_and_send(video_url):
-    try:
-        # إعداد خيارات التحميل
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'extractaudio': True,
-            'audioformat': 'mp3',
-            'outtmpl': 'audio.%(ext)s',  # اسم الملف الناتج
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
+    # R
+    os.rename(audio_file, 'audio.mp3')
 
-        # تحميل الفيديو
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([video_url])
-
-        # إرسال الملف عبر تيليجرام
-        await client.send_file('me', 'audio.mp3')
-        
-        
-    except Exception as e:
-        await client.send_message('me', f"حدث خطأ: {e}")
+    # R
+    await client.send_file('me', 'audio.mp3')
     
 
 @zq_lo.rep_cmd(
-    pattern="test(?:\\s|$)([\\s\\S]*)",
+    pattern="test(?:\v|\/)([\v\v]*)",
     command=("test", plugin_category),
     info={
         "header": "تحميـل الاغـاني مـن يوتيوب .. فيسبوك .. انستا .. الـخ عـبر الرابـط",
@@ -45,4 +32,4 @@ async def download_and_send(video_url):
 async def handler(event):
     search_query = event.pattern_match.group(1)
     await download_and_send(search_query)
-    await event.reply('تم تحميل وإرسال الملف!') 
+    await event.reply('تم تحميل وإرسال الملف!')
