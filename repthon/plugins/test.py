2e179866209dac6534087494b86f09999a0dc699
Repthon-Ar <alfashymwswlyny@gmail.com>
2024-10-14 09:02:35 +0300

    Create test.py

---
+++ repthon/plugins/test.py
@@ -0,0 +1,39 @@
+import os
+import re
+from pytube import YouTube, Search
+from telethon import events
+from repthon import zq_lo
+
+@zq_lo.on(events.NewMessage(pattern=r'.بحث3 (.+)'))
+async def download_song(event):
+    video_name = event.pattern_match.group(1)
+   if not re.match(r'(https?://)?(www.)?(youtube.com|youtu.?be)/.+$', url):
+        await event.reply("يرجى إدخال رابط يوتيوب صحيح.")
+        return
+    await event.reply(f"جاري البحث عن الفيديو: {video_name}...")
+
+    try:
+        # البحث عن الفيديو باستخدام اسم الفيديو
+        search_results = Search(video_name)
+        if search_results.results:
+            yt = search_results.results[0]  # Baq
+
+            await event.reply(f"جاري تحميل الفيديو: {yt.title}...")
+
+            audio_stream = yt.streams.filter(only_audio=True).first()
+            audio_file = audio_stream.download(filename="temp_audio")
+
+            # R
+            mp3_file = f"{yt.title}.mp3"
+            os.rename(audio_file, mp3_file)
+
+            # R
+            await event.reply(file=mp3_file)
+
+            os.remove(mp3_file)
+
+        else:
+            await event.reply("لم يتم العثور على أي فيديو بهذا الاسم.")
+            
+    except Exception as e:
+        await event.reply(f"حدث خطأ أثناء تنزيل الأغنية: {str(e)}")
