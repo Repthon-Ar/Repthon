ad520297b6a7030545e8f1e7fba6089d2eebc6b2
Repthon-Ar <alfashymwswlyny@gmail.com>
2024-10-14 09:38:24 +0300

    Update test.py

--- repthon/plugins/test.py
+++ repthon/plugins/test.py
@@ -10,7 +10,6 @@ plugin_category = "البوت"
 async def download_song(event):
     video_name = event.pattern_match.group(1)
     if not re.match(r'(https?://)?(www.)?(youtube.com|youtu.?be)/.+$', video_name):
-        await event.reply("يرجى إدخال رابط يوتيوب صحيح.")
         return
     await event.reply(f"جاري البحث عن الفيديو: {video_name}...")
 
