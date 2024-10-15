6a9d5b62c26b819a96e993dbce6c3b16914c1246
Repthon-Ar <alfashymwswlyny@gmail.com>
2024-10-15 09:14:15 +0300

    Update vcp_helper.py

--- repthon/vc_baqir/vcp_helper.py
+++ repthon/vc_baqir/vcp_helper.py
@@ -79,7 +79,7 @@ class RepVC:
         try:
             await self.app.join_group_call(
                 chat_id=chat.id,
-                stream=AudioPiped("baqir/taiba/Silence01s.mp3"),
+                stream=AudioPiped("baqir/baqir/Silence01s.mp3"),
                 join_as=join_as_chat,
                 stream_type=StreamType().pulse_stream,
             )
