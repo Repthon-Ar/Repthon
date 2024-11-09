a50190f6e6a2a61a9ef167a10378a8112e157d6e
Repthon-Ar <alfashymwswlyny@gmail.com>
2024-11-09 11:06:03 +0300

    Update البحث.py

--- repthon/plugins/البحث.py
+++ repthon/plugins/البحث.py
@@ -1,229 +1,97 @@
+"""
+❃ `{i}تحميل صوتي` <(رابط يوتيوب/او اي رابط)>
+   لـ تحميل الملف بشكل ملف صوتي في التليجرام، يمكنك وضع رابط اي منصة
+
+
+❃ `{i}تحميل فيد` <(رابط يوتيوب/او اي رابط)>
+   لـ تحميل الملف بشكل فيديو في التليجرام، يمكنك وضع رابط اي منصة
+
+
+❃ `{i}صوتي` (عنوان)>
+   لـ تحميل الملف بشكل ملف صوتي في التليجرام من خلال العنوان فقط بدون رابط
+
+ By:@RR0R7 ~ @E_7_V
+"""
+
 import asyncio
-import base64
-import random
-import io
-import urllib.parse
-import os
-import glob
-from pathlib import Path
-import asyncio
+import yt_dlp
 from asyncio import sleep
-
-from ShazamAPI import Shazam
-from telethon import types
-from telethon.errors.rpcerrorlist import YouBlockedUserError, ChatSendMediaForbiddenError
-from telethon.tl.functions.contacts import UnblockRequest as unblock
-from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from validators.url import url
-
+from youtubesearchpython import Playlist
+from yt_dlp import YoutubeDL
+from repthon import zq_lo
 from ..core.logger import logging
-from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.functions import delete_conv
-from ..helpers.functions.utube import name_dl, song_dl, video_dl, yt_search
-from ..helpers.tools import media_type
-from ..helpers.utils import _reputils, reply_id
-from . import zq_lo, song_download
+from ..helpers.functions.youtube import download_yt, get_yt_link, is_url_work
 
 plugin_category = "البحث"
 LOGS = logging.getLogger(__name__)
 
-# =========================================================== #
-#                                                             𝙍𝙀𝙋𝙏𝙃𝙊𝙉
-# =========================================================== #
-SONG_SEARCH_STRING = "<b>╮ جـارِ البحث ؏ـن الاغنيـٓه... 🎧♥️╰</b>"
-SONG_NOT_FOUND = "<b>⎉╎لـم استطـع ايجـاد المطلـوب .. جرب البحث باستخـدام الامـر (.اغنيه)</b>"
-SONG_SENDING_STRING = "<b>╮ جـارِ تحميـل الاغنيـٓه... 🎧♥️╰</b>"
-# =========================================================== #
-#                                                             𝙍𝙀𝙋𝙏𝙃𝙊𝙉
-# =========================================================== #
+ytd = {
+        "prefer_ffmpeg": True,
+        "addmetadata": True,
+        "geo-bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegMetadata"}],
+    }
 
-def get_cookies_file():
-    folder_path = f"{os.getcwd()}/rbaqir"
-    txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
-    if not txt_files:
-        raise FileNotFoundError("No .txt files found in the specified folder.")
-    cookie_txt_file = random.choice(txt_files)
-    return cookie_txt_file
-
-@zq_lo.rep_cmd(
-    pattern="بحث(320)?(?:\s|$)([\s\S+]*)",
-    command=("بحث", plugin_category),
-    info={
-        "header": "لـ تحميـل الاغـانـي مـن يـوتيـوب",
-        "امـر مضـاف": {
-            "320": "لـ البحـث عـن الاغـانـي وتحميـلهـا بـدقـه عـاليـه 320k",
-        },
-        "الاسـتخـدام": "{tr}بحث + اسـم الاغنيـه",
-        "مثــال": "{tr}.بحث Dark Beach",
-    },
-)
-async def _(event):
-    "To search songs"
-    reply_to_id = await reply_id(event)
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(2):
-        query = event.pattern_match.group(2)
-        query = f"{query} mp3"
-    elif reply and reply.message:
-        query = reply.message
-        query = f"{query} mp3"
-    else:
-        return await edit_or_reply(event, "**⎉╎قم باضافـة الاغنيـه للامـر .. بحث + اسـم الاغنيـه**")
-    repevent = await edit_or_reply(event, "**╮ جـارِ البحث ؏ـن المقطـٓع الصٓوتـي... 🎧♥️╰**")
-    video_link = await yt_search(str(query))
-    if not url(video_link):
-        return await repevent.edit(
-            f"**⎉╎عـذراً .. لـم استطـع ايجـاد** {query}"
+@zq_lo.rep_cmd(pattern="تحميل صوتي (.*)")
+async def down_voic(event):
+    bot = await event.eor("⌔∮ جار التحميل يرجى الأنتظار قليلا")
+    ytd["format"] = "bestaudio"
+    ytd["outtmpl"] = "%(id)s.m4a"
+    ytd["postprocessors"].insert(
+            0,
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "m4a",
+                "preferredquality": "128",
+            },
         )
-    cmd = event.pattern_match.group(1)
-    q = "320k" if cmd == "320" else "128k"
-    song_cmd = song_dl.format(QUALITY=q, video_link=video_link)
-    name_cmd = name_dl.format(video_link=video_link)
+    url = event.pattern_match.group(1)
+    if not url:
+        return await bot.eor("⌔∮ يجب عليك وضع رابط للتحميل الصوتي")
     try:
-        stderr = (await _reputils.runcmd(song_cmd))[1]
-        await runcmd(f"yt-dlp --cookies {get_cookies_file()}")
-        # if stderr:
-        # await repevent.edit(f"**Error1 :** `{stderr}`")
-        catname, stderr = (await _reputils.runcmd(name_cmd))[:2]
-        if stderr:
-            return await repevent.edit(f"**خطــأ :** `{stderr}`")
-        catname = os.path.splitext(catname)[0]
-        song_file = Path(f"{catname}.mp3")
-    except:
-        pass
-    if not os.path.exists(song_file):
-        return await repevent.edit(
-            f"**⎉╎عـذراً .. لـم استطـع ايجـاد** {query}"
-        )
-    await repevent.edit("**╮ ❐ جـارِ التحميـل انتظـر قليلاً  ▬▭... 𓅫╰**")
-    catthumb = Path(f"{catname}.jpg")
-    if not os.path.exists(catthumb):
-        catthumb = Path(f"{catname}.webp")
-    elif not os.path.exists(catthumb):
-        catthumb = None
-    title = catname.replace("./temp/", "").replace("_", "|")
-    try:
-        await event.client.send_file(
-            event.chat_id,
-            song_file,
-            force_document=False,
-            caption=f"**⎉╎البحث :** `{title}`",
-            thumb=catthumb,
-            supports_streaming=True,
-            reply_to=reply_to_id,
-        )
-        await repevent.delete()
-        for files in (catthumb, song_file):
-            if files and os.path.exists(files):
-                os.remove(files)
-    except ChatSendMediaForbiddenError as err: # Code By T.me/E_7_V
-        await repevent.edit("**- عـذراً .. الوسـائـط مغلقـه هنـا ؟!**")
-        LOGS.error(str(err))
-
-
-@zq_lo.rep_cmd(
-    pattern="فيديو(?:\s|$)([\s\S]*)",
-    command=("فيديو", plugin_category),
-    info={
-        "header": "لـ تحميـل مقـاطـع الفيـديـو مـن يـوتيـوب",
-        "الاسـتخـدام": "{tr}فيديو + اسـم المقطـع",
-        "مثــال": "{tr}فيديو حالات واتس",
-    },
-)
-async def _(event):
-    "لـ تحميـل مقـاطـع الفيـديـو مـن يـوتيـوب"
-    reply_to_id = await reply_id(event)
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-    elif reply and reply.message:
-        query = reply.message
-    else:
-        return await edit_or_reply(event, "**⎉╎قم باضافـة الاغنيـه للامـر .. فيديو + اسـم الفيديـو**")
-    cat = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-    repevent = await edit_or_reply(event, "**╮ جـارِ البحث ؏ـن الفيديـو... 🎧♥️╰**")
-    video_link = await yt_search(str(query))
-    if not url(video_link):
-        return await repevent.edit(
-            f"**⎉╎عـذراً .. لـم استطـع ايجـاد** {query}"
-        )
-    try:
-        cat = Get(cat)
-        await event.client(cat)
+        await is_url_work(url)
     except BaseException:
-        pass
-    name_cmd = name_dl.format(video_link=video_link)
-    video_cmd = video_dl.format(video_link=video_link)
-    try:
-        stderr = (await _reputils.runcmd(video_cmd))[1]
-        # if stderr:
-        # return await repevent.edit(f"**Error :** `{stderr}`")
-        repname, stderr = (await _reputils.runcmd(name_cmd))[:2]
-        if stderr:
-            return await repevent.edit(f"**خطأ :** `{stderr}`")
-        repname = os.path.splitext(repname)[0]
-        vsong_file = Path(f"{repname}.mp4")
-    except:
-        pass
-    if not os.path.exists(vsong_file):
-        vsong_file = Path(f"{repname}.mkv")
-    elif not os.path.exists(vsong_file):
-        return await repevent.edit(
-            f"**⎉╎عـذراً .. لـم استطـع ايجـاد** {query}"
-        )
-    await repevent.edit("**- جـارِ التحميـل انتظـر ▬▭...**")
-    repthumb = Path(f"{repname}.jpg")
-    if not os.path.exists(repthumb):
-        repthumb = Path(f"{repname}.webp")
-    elif not os.path.exists(repthumb):
-        repthumb = None
-    title = repname.replace("./temp/", "").replace("_", "|")
-    await event.client.send_file(
-        event.chat_id,
-        vsong_file,
-        caption=f"**⎉╎البحث :** `{title}`",
-        thumb=repthumb,
-        supports_streaming=True,
-        reply_to=reply_to_id,
-    )
-    await repevent.delete()
-    for files in (repthumb, vsong_file):
-        if files and os.path.exists(files):
-            os.remove(files)
+        return await bot.eor("⌔∮ يرجى وضع الرابط بشكل صحيح")
+    await download_yt(bot, url, ytd)
 
-@zq_lo.rep_cmd(
-    pattern="بحث2(?:\s|$)([\s\S]*)",
-    command=("بحث2", plugin_category),
-    info={
-        "header": "To search songs and upload to telegram",
-        "description": "Searches the song you entered in query and sends it quality of it is 320k",
-        "usage": "{tr}song2 <song name>",
-        "examples": "{tr}song2 memories",
-    },
-)
-async def song2(event):
-    "To search songs"
-    song = event.pattern_match.group(1)
-    chat = "@CatMusicRobot"
-    reply_id_ = await reply_id(event)
-    catevent = await edit_or_reply(event, SONG_SEARCH_STRING, parse_mode="html")
-    async with event.client.conversation(chat) as conv:
-        try:
-            purgeflag = await conv.send_message(song)
-        except YouBlockedUserError:
-            await zq_lo(unblock("CatMusicRobot"))
-            purgeflag = await conv.send_message(song)
-        music = await conv.get_response()
-        await event.client.send_read_acknowledge(conv.chat_id)
-        if not music.media:
-            return await edit_delete(catevent, SONG_NOT_FOUND, parse_mode="html")
-        await event.client.send_read_acknowledge(conv.chat_id)
-        await event.client.send_file(
-            event.chat_id,
-            music,
-            caption=f"<b>⎉╎البحث : <code>{song}</code></b>",
-            parse_mode="html",
-            reply_to=reply_id_,
+@zq_lo.rep_cmd(pattern="تحميل فيد (.*)")
+async def vidown(event):
+    bot = await event.eor("⌔∮ جار التحميل يرجى الأنتظار قليلا")
+    ytd["format"] = "best"
+    ytd["outtmpl"] = "%(id)s.mp4"
+    ytd["postprocessors"].insert(
+        0, {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
         )
-        await catevent.delete()
-        await delete_conv(event, chat, purgeflag)
+    url = event.pattern_match.group(1)
+    print(url)
+    if not url:
+        return await bot.eor("⌔∮ يجب عليك وضع رابط لتحميل الفيد")
+    try:
+        await is_url_work(url)
+    except BaseException:
+        return await bot.eor("⌔∮ يرجى وضع الرابط بشكل صحيح")
+    await download_yt(bot, url, ytd)
+
+
+@zq_lo.rep_cmd(pattern="بحث( (.*)|$)")
+async def sotea(event):
+    bot = await event.eor("⌔∮ جار التحميل يرجى الأنتظار قليلا")
+    ytd["format"] = "bestaudio"
+    ytd["outtmpl"] = "%(id)s.m4a"
+    ytd["postprocessors"].insert(
+        0,
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "m4a",
+            "preferredquality": "128",
+        },
+    )
+    query = event.pattern_match.group(2) if event.pattern_match.group(1) else None
+    if not query:
+        return await bot.eor("**⌔∮ يجب عليك تحديد ما تريد تحميله اكتب عنوان مع الامر**")
+    url = get_yt_link(query, ytd)
+    if not url:
+        return await bot.edit("**⌔∮ لم يتم العثور على الفيديو اكتب عنوان مفصل بشكل صحيح**")
+    await bot.eor("**⌔∮ جار تحميل الملف الصوتي أنتظر قليلا**")
+    await download_yt(bot, url, ytd)
