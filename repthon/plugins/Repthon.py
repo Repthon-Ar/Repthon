2fd7b6259ef5aecddb2bcc0aee178c8dd4c9170b
Repthon-Ar <alfashymwswlyny@gmail.com>
2025-08-22 09:44:24 +0300

    Update Repthon.py

--- repthon/plugins/Repthon.py
+++ repthon/plugins/Repthon.py
@@ -10,10 +10,18 @@ from uuid import uuid4
 from telethon import Button, types
 from telethon.errors import QueryIdInvalidError
 from telethon.events import CallbackQuery, InlineQuery
+from youtubesearchpython import VideosSearch
 
 from . import zq_lo
 from ..Config import Config
 from ..helpers.functions import rand_key
+from ..helpers.functions.utube import (
+    download_button,
+    get_yt_video_id,
+    get_ytthumb,
+    result_formatter,
+    ytsearch_data,
+)
 from ..sql_helper.globals import gvarstatus
 from ..core.logger import logging
 
@@ -42,7 +50,75 @@ async def inline_handler(event):  # sourcery no-metrics
     string.split()
     query_user_id = event.query.user_id
     if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS:
-        if string == "pmpermit":
+        if str_y[0].lower() == "ytdl" and len(str_y) == 2:
+            link = get_yt_video_id(str_y[1].strip())
+            found_ = True
+            if link is None:
+                search = VideosSearch(str_y[1].strip(), limit=15)
+                resp = (search.result()).get("result")
+                if len(resp) == 0:
+                    found_ = False
+                else:
+                    outdata = await result_formatter(resp)
+                    key_ = rand_key()
+                    ytsearch_data.store_(key_, outdata)
+                    buttons = [
+                        Button.inline(
+                            f"1 / {len(outdata)}",
+                            data=f"ytdl_next_{key_}_1",
+                        ),
+                        Button.inline(
+                            "القائمـة 📜",
+                            data=f"ytdl_listall_{key_}_1",
+                        ),
+                        Button.inline(
+                            "⬇️  تحميـل",
+                            data=f'ytdl_download_{outdata[1]["video_id"]}_0',
+                        ),
+                    ]
+                    caption = outdata[1]["message"]
+                    photo = await get_ytthumb(outdata[1]["video_id"])
+            else:
+                caption, buttons = await download_button(link, body=True)
+                photo = await get_ytthumb(link)
+            if found_:
+                markup = event.client.build_reply_markup(buttons)
+                photo = types.InputWebDocument(
+                    url=photo, size=0, mime_type="image/jpeg", attributes=[]
+                )
+                text, msg_entities = await event.client._parse_message_text(
+                    caption, "html"
+                )
+                result = types.InputBotInlineResult(
+                    id=str(uuid4()),
+                    type="photo",
+                    title=link,
+                    description="⬇️ اضغـط للتحميـل",
+                    thumb=photo,
+                    content=photo,
+                    send_message=types.InputBotInlineMessageMediaAuto(
+                        reply_markup=markup, message=text, entities=msg_entities
+                    ),
+                )
+            else:
+                result = builder.article(
+                    title="Not Found",
+                    text=f"No Results found for `{str_y[1]}`",
+                    description="INVALID",
+                )
+            try:
+                await event.answer([result] if result else None)
+            except QueryIdInvalidError:
+                await event.answer(
+                    [
+                        builder.article(
+                            title="Not Found",
+                            text=f"No Results found for `{str_y[1]}`",
+                            description="INVALID",
+                        )
+                    ]
+                )
+        elif string == "pmpermit":
             controlpmch = gvarstatus("pmchannel") or None
             if controlpmch is not None:
                 rchannel = controlpmch.replace("@", "")
