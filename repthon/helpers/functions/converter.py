5ee454728d6b6eebc2988ce72442c30ba9aba718
Repthon-Ar <alfashymwswlyny@gmail.com>
2025-10-26 09:42:45 +0300

    Update converter.py

--- repthon/helpers/functions/converter.py
+++ repthon/helpers/functions/converter.py
@@ -2,11 +2,11 @@ import os
 
 from PIL import Image
 
-from repthon.core.logger import logging
-from repthon.core.managers import edit_or_reply
-from repthon.helpers.functions.vidtools import take_screen_shot
-from repthon.helpers.tools import fileinfo, media_type, meme_type
-from repthon.helpers.utils.utils import runcmd
+from ...core.logger import logging
+from ...core.managers import edit_or_reply
+from ...helpers.functions.vidtools import take_screen_shot
+from ...helpers.tools import fileinfo, media_type, meme_type
+from ...helpers.utils.utils import runcmd
 
 LOGS = logging.getLogger(__name__)
 
@@ -19,42 +19,50 @@ class CatConverter:
         if os.path.exists(catfile):
             os.remove(catfile)
         try:
-            catmedia = reply if os.path.exists(reply) else None
+            repmedia = reply if os.path.exists(reply) else None
         except TypeError:
             if memetype in ["Video", "Gif"]:
-                dirct = "./temp/repthon.mp4"
+                dirct = "./temp/repfile.mp4"
             elif memetype == "Audio":
-                dirct = "./temp/repthon.mp3"
-            catmedia = await reply.download_media(dirct)
-        return catfile, catmedia
+                dirct = "./temp/repfile.mp3"
+            repmedia = await reply.download_media(dirct)
+        return catfile, repmedia
 
-    async def to_image(self, event, reply, dirct="./temp", file="meme.png", noedits=False, rgb=False):
+    async def to_image(
+        self, event, reply, dirct="./temp", file="meme.png", noedits=False, rgb=False
+    ):
         memetype = await meme_type(reply)
         mediatype = await media_type(reply)
         if memetype == "Document":
             return event, None
-        catevent = event if noedits else await edit_or_reply(event, "`Transfiguration Time! Converting to ....`")
-        catfile, catmedia = await self._media_check(reply, dirct, file, memetype)
+        catevent = (
+            event
+            if noedits
+            else await edit_or_reply(
+                event, "`Transfiguration Time! Converting to ....`"
+            )
+        )
+        catfile, repmedia = await self._media_check(reply, dirct, file, memetype)
         if memetype == "Photo":
-            im = Image.open(catmedia)
+            im = Image.open(repmedia)
             im.save(catfile)
         elif memetype in ["Audio", "Voice"]:
-            await runcmd(f"ffmpeg -i '{catmedia}' -an -c:v copy '{catfile}' -y")
+            await runcmd(f"ffmpeg -i '{repmedia}' -an -c:v copy '{catfile}' -y")
         elif memetype in ["Round Video", "Video", "Gif"]:
-            await take_screen_shot(catmedia, "00.00", catfile)
+            await take_screen_shot(zedmedia, "00.00", catfile)
         if mediatype == "Sticker":
             if memetype == "Animated Sticker":
-                catcmd = f"lottie_convert.py --frame 0 -if lottie -of png '{catmedia}' '{catfile}'"
+                catcmd = f"lottie_convert.py --frame 0 -if lottie -of png '{repmedia}' '{catfile}'"
                 stdout, stderr = (await runcmd(catcmd))[:2]
                 if stderr:
                     LOGS.info(stdout + stderr)
             elif memetype == "Video Sticker":
-                await take_screen_shot(catmedia, "00.00", catfile)
+                await take_screen_shot(repmedia, "00.00", catfile)
             elif memetype == "Static Sticker":
-                im = Image.open(catmedia)
+                im = Image.open(repmedia)
                 im.save(catfile)
-        if catmedia and os.path.exists(catmedia):
-            os.remove(catmedia)
+        if repmedia and os.path.exists(repmedia):
+            os.remove(repmedia)
         if os.path.exists(catfile):
             if rgb:
                 img = Image.open(catfile)
@@ -64,7 +72,9 @@ class CatConverter:
             return catevent, catfile, mediatype
         return catevent, None
 
-    async def to_sticker(self, event, reply, dirct="./temp", file="meme.webp", noedits=False, rgb=False):
+    async def to_sticker(
+        self, event, reply, dirct="./temp", file="meme.webp", noedits=False, rgb=False
+    ):
         filename = os.path.join(dirct, file)
         response = await self.to_image(event, reply, noedits=noedits, rgb=rgb)
         if response[1]:
@@ -74,7 +84,9 @@ class CatConverter:
             return response[0], filename, response[2]
         return response[0], None
 
-    async def to_webm(self, event, reply, dirct="./temp", file="animate.webm", noedits=False):
+    async def to_webm(
+        self, event, reply, dirct="./temp", file="animate.webm", noedits=False
+    ):
         # //Hope u dunt kang :/ @Jisan7509
         memetype = await meme_type(reply)
         if memetype not in [
@@ -84,18 +96,28 @@ class CatConverter:
             "Video",
         ]:
             return event, None
-        catevent = event if noedits else await edit_or_reply(event, "__🎞Converting into Animated sticker..__")
-        catfile, catmedia = await self._media_check(reply, dirct, file, memetype)
-        media = await fileinfo(catmedia)
+        catevent = (
+            event
+            if noedits
+            else await edit_or_reply(event, "__🎞Converting into Animated sticker..__")
+        )
+        catfile, repmedia = await self._media_check(reply, dirct, file, memetype)
+        media = await fileinfo(repmedia)
         h = media["height"]
         w = media["width"]
         w, h = (-1, 512) if h > w else (512, -1)
-        await runcmd(f"ffmpeg -to 00:00:02.900 -i '{catmedia}' -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an '{catfile}'")  # pain
-        if os.path.exists(catmedia):
-            os.remove(catmedia)
-        return (catevent, catfile) if os.path.exists(catfile) else (catevent, None)
+        await runcmd(
+            f"ffmpeg -to 00:00:02.900 -i '{repmedia}' -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an '{catfile}'"
+        )  # pain
+        if os.path.exists(repmedia):
+            os.remove(repmedia)
+        if os.path.exists(catfile):
+            return catevent, catfile
+        return catevent, None
 
-    async def to_gif(self, event, reply, dirct="./temp", file="meme.mp4", maxsize="5M", noedits=False):
+    async def to_gif(
+        self, event, reply, dirct="./temp", file="meme.mp4", maxsize="5M", noedits=False
+    ):
         memetype = await meme_type(reply)
         mediatype = await media_type(reply)
         if memetype not in [
@@ -106,20 +128,30 @@ class CatConverter:
             "Gif",
         ]:
             return event, None
-        catevent = event if noedits else await edit_or_reply(event, "`Transfiguration Time! Converting to ....`")
-        catfile, catmedia = await self._media_check(reply, dirct, file, memetype)
+        catevent = (
+            event
+            if noedits
+            else await edit_or_reply(
+                event, "`Transfiguration Time! Converting to ....`"
+            )
+        )
+        catfile, repmedia = await self._media_check(reply, dirct, file, memetype)
         if mediatype == "Sticker":
             if memetype == "Video Sticker":
-                await runcmd(f"ffmpeg -i '{catmedia}' -c copy '{catfile}'")
+                await runcmd(f"ffmpeg -i '{repmedia}' -c copy '{catfile}'")
             elif memetype == "Animated Sticker":
-                await runcmd(f"lottie_convert.py '{catmedia}' '{catfile}'")
-        if catmedia.endswith(".gif"):
-            await runcmd(f"ffmpeg -f gif -i '{catmedia}' -fs {maxsize} -an '{catfile}'")
+                await runcmd(f"lottie_convert.py '{repmedia}' '{catfile}'")
+        if repmedia.endswith(".gif"):
+            await runcmd(f"ffmpeg -f gif -i '{repmedia}' -fs {maxsize} -an '{catfile}'")
         else:
-            await runcmd(f"ffmpeg -i '{catmedia}' -c:v libx264 -fs {maxsize} -an '{catfile}'")
-        if catmedia and os.path.exists(catmedia):
-            os.remove(catmedia)
-        return (catevent, catfile) if os.path.exists(catfile) else (catevent, None)
+            await runcmd(
+                f"ffmpeg -i '{repmedia}' -c:v libx264 -fs {maxsize} -an '{catfile}'"
+            )
+        if repmedia and os.path.exists(repmedia):
+            os.remove(repmedia)
+        if os.path.exists(catfile):
+            return catevent, catfile
+        return catevent, None
 
 
 Convert = CatConverter()
